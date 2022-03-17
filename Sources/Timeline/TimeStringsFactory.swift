33a0c63ffa84ed7123660855ca8058a47f56b936
Stephen Grant <stephengrant@miixcrooz.com>
2022-03-17 20:43:42 +0200

    Revert "8AM  to 5PM changes"
    This reverts commit cc6f64d25c4c5da8b321e023a2b231dee557fd7d.

--- Sources/Timeline/TimeStringsFactory.swift
+++ Sources/Timeline/TimeStringsFactory.swift
@@ -22,27 +22,20 @@ struct TimeStringsFactory {
   }
 
   func make12hStrings() -> [String] {
-      var numbers = [String]()
-      var pmnumbers = [String]()
-    //numbers.append("12")
+    var numbers = [String]()
+    numbers.append("12")
 
-    //  for i in 8...11 {
-    for i in 8...11 {
+    for i in 1...11 {
       let string = String(i)
       numbers.append(string)
     }
-      
-    for i in 1...5 {
-        let string = String(i)
-        pmnumbers.append(string)
-    }
 
     var am = numbers.map { $0 + " " + calendar.amSymbol}
-    var pm = pmnumbers.map { $0 + " " + calendar.pmSymbol}
+    var pm = numbers.map { $0 + " " + calendar.pmSymbol}
     
     am.append(localizedString("12:00"))
-    //pm.removeFirst()
-    //pm.append(am.first!)
+    pm.removeFirst()
+    pm.append(am.first!)
     return am + pm
   }
 }
