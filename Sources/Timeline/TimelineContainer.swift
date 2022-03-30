541fd39ab80bc05fe8adff1c64a298e27612019b
Stephen Grant <stephengrant@miixcrooz.com>
2022-03-30 11:32:04 +0200

    8 AM to 5 PM fix

--- Sources/Timeline/TimelineContainer.swift
+++ Sources/Timeline/TimelineContainer.swift
@@ -67,7 +67,7 @@ public final class TimelineContainer: UIScrollView {
 //        let padding: CGFloat = 8
         //setTimelineOffset(CGPoint(x: contentOffset.x, y:  CGFloat(yVal)), animated: true)
         
-        let desiredOffset = CGPoint(x: 0, y: -contentInset.top)
+        let desiredOffset = CGPoint(x: contentOffset.x, y: -contentInset.top)
         setContentOffset(desiredOffset, animated: true)
 
         //setContentOffset(CGPoint(x: contentOffset.x, y: CGFloat(CFloat(yVal))), animated: true)
